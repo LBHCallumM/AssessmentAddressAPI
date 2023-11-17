7fe2946cdbc0c38007f4ea71649a9683f780cf1b
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-11-17 11:42:32 +0000

    Scaffold address gateway

---
+++ AssessmentAddressAPI/Gateways/AddressGateway.cs
@@ -0,0 +1,13 @@
+﻿using AssessmentAddressAPI.Gateways.Interfaces;
+using AssessmentAddressAPI.Infrastructure;
+
+namespace AssessmentAddressAPI.Gateways
+{
+    public class AddressGateway : IAddressGateway
+    {
+        public IEnumerable<HackneyAddress> GetAddressesByPostCode(string postCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AssessmentAddressAPI/Gateways/Interfaces/IAddressGateway.cs
@@ -0,0 +1,10 @@
+﻿using AssessmentAddressAPI.Infrastructure;
+
+namespace AssessmentAddressAPI.Gateways.Interfaces
+{
+    public interface IAddressGateway
+    {
+        public IEnumerable<HackneyAddress> GetAddressesByPostCode(string postCode);
+
+    }
+}
