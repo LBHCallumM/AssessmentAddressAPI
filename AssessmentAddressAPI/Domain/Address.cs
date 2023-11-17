e040239827edf4be51866eb233131f4116457eef
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2023-11-17 12:20:16 +0000

    Add Address domain class and factory method

---
+++ AssessmentAddressAPI/Domain/Address.cs
@@ -0,0 +1,21 @@
+﻿namespace AssessmentAddressAPI.Domain
+{
+    public class Address
+    {
+        public long Uprn { get; set; }
+
+
+        public string Street { get; set; }
+        public string BuildingNumber { get; set; }
+
+        public string Locality { get; set; }
+        public string Ward { get; set; }
+        public string Town { get; set; }
+        public string PostCode { get; set; }
+
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string AddressLine4 { get; set; }
+    }
+}
---
+++ AssessmentAddressAPI/Factories/DbToDomainFactory.cs
@@ -0,0 +1,28 @@
+﻿using AssessmentAddressAPI.Domain;
+using AssessmentAddressAPI.Infrastructure;
+using System.Runtime.CompilerServices;
+
+namespace AssessmentAddressAPI.Factories
+{
+    public static class DbToDomainFactory
+    {
+        public static Address ToDomain(this HackneyAddress db)
+        {
+            return new Address
+            {
+                Uprn = db.Uprn,
+                Street = db.StreetDescription ?? "",
+                BuildingNumber = db.BuildingNumber ?? "",
+                Locality = db.Locality ?? "",
+                Ward = db.Ward ?? "",
+                Town = db.Town ?? "",
+                PostCode = db.Postcode ?? "",
+                AddressLine1 = db.Line1 ?? "",
+                AddressLine2 = db.Line2 ?? "",
+                AddressLine3 = db.Line3 ?? "",
+                AddressLine4 = db.Line4 ?? "",
+            };
+        }
+
+    }
+}
